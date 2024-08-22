a5517bfbf43da182a7d0d2e6cbbb71ab55f0f3e7
suriyakalababu <suriyakalababu2201@gmail.com>
2024-08-22 21:49:26 +0530

    Add files via upload
    week1

---
+++ week1Day2/Mobile.java
@@ -0,0 +1,27 @@
+package week1Day2;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		
+		String mobileName="samsang";
+		String mobileModel="S24";
+		int mobilePrice=30000;
+		float mobileOffers=5.12345f;
+		double mobieSARvalue=456655.987776660;
+		char mobieleLogo='S';
+		Long imeNumber=67789064347653478l;
+		System.out.println(mobileName);
+		System.out.println(mobileModel);
+		System.out.println(mobilePrice);
+		
+		
+		
+		System.out.println(mobileName+" ,"+mobileModel+" ,"+mobilePrice+" ,");
+	
+	
+
+
+	}
+
+}
---
+++ week1Day2/main.java
@@ -0,0 +1,26 @@
+package week1Day2;
+
+public class main {
+
+	private void applingBreak() {
+		// TODO Auto-generated method stub
+System.out.println("Break has been applied");
+	}
+	
+	public void soundHorn() {
+		System.out.println("Sound Horn");
+			
+		}
+	
+	public static void main(String[] args) {
+	
+		// TODO Auto-generated method stub
+		main obj=new main();
+		obj.applingBreak();
+		obj.soundHorn();
+		
+		
+
+	}
+
+}
