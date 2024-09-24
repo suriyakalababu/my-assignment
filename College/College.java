99923877f410c48a3e24849b1c97d458101b6443
suriyakalababu <suriyakalababu2201@gmail.com>
2024-09-25 00:16:52 +0530

    Add files via upload

---
+++ College/College.java
@@ -0,0 +1,27 @@
+package org.College;
+
+public class College {
+
+	public void collagename() {
+		System.out.println("Peri Engineering college");
+
+	}
+
+	public void collegeCode() {
+		System.out.println("109");
+
+	}
+
+	public void collegeRank() {
+		System.out.println("Bestplacement in tamilnadu");
+	}
+
+	public static void main(String[] args) {
+		College obj = new College();
+		obj.collagename();
+		obj.collegeCode();
+		obj.collegeRank();
+
+	}
+
+}
