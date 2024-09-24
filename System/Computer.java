8c6f77cded367dc03560bae215c2053b86f30862
suriyakalababu <suriyakalababu2201@gmail.com>
2024-09-25 00:23:03 +0530

    Add files via upload

---
+++ System/Computer.java
@@ -0,0 +1,16 @@
+package org.System;
+
+public class Computer {
+	public void computermodel() {
+		System.out.println("DELL");
+	}
+
+	public static void main(String[] args) {
+		
+		 Computer model=new  Computer ();
+		 model.computermodel();
+		 
+
+	}
+
+}
---
+++ System/Desktop.java
@@ -0,0 +1,17 @@
+package org.System;
+
+public class Desktop extends Computer {
+	
+public void desktopsize() {
+	System.out.println("Linux");
+}
+	public static void main(String[] args) {
+		Desktop obj = new Desktop();
+		obj.computermodel();
+		obj.desktopsize();
+		
+		
+		
+	}
+
+}
